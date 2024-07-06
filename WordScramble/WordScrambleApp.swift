35b01014da645e63f8692ed7a1be2326c248d7d7
bbaneydev <baneyjb@gmail.com>
2024-07-06 11:08:38 -0500

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Billy Baney on 7/6/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
